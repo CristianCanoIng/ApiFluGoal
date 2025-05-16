337ef811640bdd357970a66de55308610caa78db
CristianCanoIng <canox09@gmail.com>
2025-05-15 21:30:16 -0500

    nnr

--- src/main/java/com/example/FluGoal/model/Usuario.java
+++ src/main/java/com/example/FluGoal/model/Usuario.java
@@ -12,7 +12,6 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
     private String nombre;
     @Column(unique = true)
     private String email;
